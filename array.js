b74f882324fd956fc08fdcc712b4d89efc4fcbcf
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2024-10-13 17:07:12 -0400

    Committing code after fixing a few bugs with variable scope and declaration.

---
+++ array.js
@@ -0,0 +1,26 @@
+const cardArr = [
+    {
+        question: "What are the three components of a traditional for loop?",
+        options:
+            [
+                { label: "A", text: "The Initialization Expression, the Termination Condition, and the Final Expression" },
+                { label: "B", text: "The Declaration Statement, the Execution Block, and the Iteration Variable" },
+                { label: "C", text: "The Start Condition, the Loop Body, and the End Condition" },
+                { label: "D", text: "The Initialization Phase, the Processing Phase, and the Termination Phase" }
+            ],
+        correctAnswerLabel: "A"
+    },
+    {
+        question: "What does Array.prototype.push() do?",
+        options:
+            [
+                { label: "A", text: "It removes the last element from an array and returns that element." },
+                { label: "B", text: "It adds one or more elements to the beginning of an array and returns the new length of the array." },
+                { label: "C", text: "It adds one or more elements to the end of an array and returns the new length of the array." },
+                { label: "D", text: "It merges two or more arrays into a single array and returns the new combined array." }
+            ],
+        correctAnswerLabel: "C"
+    }
+];
+
+export default cardArr;
--- logic.js
+++ logic.js
@@ -1,3 +1,4 @@
+import cardArr from "./array.js";
 const questionContainer = document.getElementById("flash-card-question");
 const questionOptions = document.getElementById("question-options");
 const startButton = document.getElementById("start-btn");
@@ -6,33 +7,11 @@ const newBtnDiv = document.getElementById("new-btn");
 const submitButton = document.createElement('button');
 const answerSubmit = document.getElementById('submit-btn')
 
+let randomIndex;
 let correctScore = 0;
 let wrongScore = 0;
 
-const cardArr = [
-    {
-        question: "What are the three components of a traditional for loop?",
-        options:
-            [
-                { label: "A", text: "The Initialization Expression, the Termination Condition, and the Final Expression" },
-                { label: "B", text: "The Declaration Statement, the Execution Block, and the Iteration Variable" },
-                { label: "C", text: "The Start Condition, the Loop Body, and the End Condition" },
-                { label: "D", text: "The Initialization Phase, the Processing Phase, and the Termination Phase" }
-            ],
-        correctAnswerLabel: "A"
-    },
-    {
-        question: "What does Array.prototype.push() do?",
-        options:
-            [
-                { label: "A", text: "It removes the last element from an array and returns that element." },
-                { label: "B", text: "It adds one or more elements to the beginning of an array and returns the new length of the array." },
-                { label: "C", text: "It adds one or more elements to the end of an array and returns the new length of the array." },
-                { label: "D", text: "It merges two or more arrays into a single array and returns the new combined array." }
-            ],
-        correctAnswerLabel: "C"
-    }
-];
+
 
 startButton.addEventListener('click', () => {
     questionContainer.style.display = 'none';
